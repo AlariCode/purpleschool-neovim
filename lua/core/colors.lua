94c8bff7ab583dcdbb740c32464253af6c476e75
Alaricode <antonlarichev@gmail.com>
2023-04-19 14:37:59 +0300

    Доработки

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -2,19 +2,20 @@ vim.opt.termguicolors = true
 
 
 require("catppuccin").setup {
-    color_overrides = {
-        mocha = {
-			base = '#16112F';
-			mantle = '#0E0A1E';
-			crust = '#0E0A1E';
-		},
-    },
+	flavour = "macchiato",
+    -- color_overrides = {
+    --     mocha = {
+	-- 		base = '#16112F';
+	-- 		mantle = '#0E0A1E';
+	-- 		crust = '#0E0A1E';
+	-- 	},
+    -- },
 	 integrations = {
         telescope = true
     }
 }
 function SetColor(color)
-    color = color or "catppuccin-mocha" -- have a default value
+    color = color or "catppuccin" -- have a default value
     vim.cmd.colorscheme(color)
 
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
