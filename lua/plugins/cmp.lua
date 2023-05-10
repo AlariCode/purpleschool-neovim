a8bd0fd0cccdda313e58b03f78ed6fffb33949ab
Alaricode <antonlarichev@gmail.com>
2023-05-10 19:27:35 +0300

    fix

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,7 +4,7 @@ cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
