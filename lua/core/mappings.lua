6ef395fecab75431a8ad9f7e81f92f58ab735fc7
Alaricode <antonlarichev@gmail.com>
2023-05-12 16:31:28 +0300

    Neotree reveal

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
 -- NeoTree
-vim.keymap.set('n', '<leader>e', ':Neotree float focus<CR>')
+vim.keymap.set('n', '<leader>e', ':Neotree float reveal<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
 
 -- Navigation
